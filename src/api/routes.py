4ee776af6b356cbc5399b5fd567529660ce37db0
Gisela94 <gisela.garrido@hotmail.com>
2024-12-10 13:01:00 +0000

    First commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,11 +5,100 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from datetime import timedelta
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
+
+# Crear una ruta para la autenticación de los usuarios y devolver un TOKEN JWT
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    if not email or not password:
+        return jsonify({"msg": "Email y contraseña son requeridos"}), 400
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if user is None:
+        # The user was not found on the database
+        return jsonify({"msg": "Usuario o contraseña incorrecto"})
+    
+    if email != user.email and password != user.password:
+        return jsonify({"msg": "E-mail o contraseña incorrectos"}), 401
+
+    
+    # Esta función lo que hacer es crear el TOKEN
+    access_token = create_access_token(identity=user.id, expires_delta=timedelta(hours=12))
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    if not email or not password:
+        return jsonify({"msg": "Missing requiered fields"}), 400
+    
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        return jsonify({"msg": "El usuario ya existe"}), 409
+    
+    new_user = User(email=email, password=password, is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify({"access_token": access_token, "user":new_user.serialize()}), 201
+
+@api.route('/update', methods=['PUT'])
+@jwt_required()
+def update_user():
+    current_user = get_jwt_identity()
+
+    user = User.query.filter_by(email= current_user.id).first()
+
+    if user is None:
+        return jsonify({"msg": "Usuario no encontrado"}), 404
+    
+    name = request.json.get("name", user.name)
+    password = request.json.get("password", user.password)
+
+    #ACTUALIZAMOS CAMPOS
+
+    user.name = name
+    user.password = password
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'msg':"Usuario actualizado"}), 200
+
+@api.route('/private', methods=['GET'])
+@jwt_required()
+def private():
+
+    #validate identity
+    current_user = get_jwt_identity()
+
+    user = User.query.get(current_user)
+
+    return jsonify(user.serialize()), 200
+
+
+
+@api.route('/users', methods=['GET'])
+@jwt_required()
+def get_users():
+    users = User.query.all()
+    result = [user.serialize() for user in users]
+    current_user_id = get_jwt_identity()
+    print(current_user_id)
+    return jsonify({'users' : result}), 200
 
 
 @api.route('/hello', methods=['POST', 'GET'])
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,8 +4,10 @@ import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Private from "./pages/private";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -17,7 +19,7 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <div>
@@ -26,11 +28,11 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<Signup />} path="/signup" />
+                        <Route element={<Private />} path="/private" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
-                    <Footer />
                 </ScrollToTop>
             </BrowserRouter>
         </div>
